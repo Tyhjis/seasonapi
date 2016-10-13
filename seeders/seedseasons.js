f2e05098973081f01bd462cd90ddf88bfec9eb7d
Kristian Hansson <khansson@Kristians-MBP.lan>
2016-10-13 15:42:54 +0300

    seedseasons made

---
+++ seeders/seedseasons.js
@@ -0,0 +1,13 @@
+var models = require('../models');
+var seed = require('./dbseed.json');
+
+var seasons = [];
+for(var key in seed.seasons) {
+  seasons.push({name: key});
+}
+models.season.bulkCreate(seasons)
+  .then(function() {
+    console.log("Veggies created");
+  }).catch(function(error) {
+    console.log(error);
+  });
